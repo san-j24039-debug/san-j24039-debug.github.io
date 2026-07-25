8ad29e7826a2986905a0d5d4f13d1059ae000182
penguin <san-j24039@sangi.jp>
2026-07-25 23:47:32 +0900

    Add practical app upgrades

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,19 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installed");
+const CACHE_VERSION = "chronex-v20260725-2";
+
+self.addEventListener("install", () => {
+    console.log("Service Worker installed:", CACHE_VERSION);
 });
 
-// キャッシュは最低限
+self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("message", (event) => {
+    if(event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
 self.addEventListener("fetch", (event) => {
     event.respondWith(fetch(event.request));
 });
